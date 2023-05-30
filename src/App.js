47512add85385de6bf2e38aa27c0d8d0355fefc6
sofialbum <sofia_1.6@hotmail.com>
2023-05-30 13:40:28 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import AuthContext from './store/auth-context';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
@@ -14,25 +15,29 @@ function App() {
   };
 
   useEffect(() => {
-   const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
-   if(storedUserLoggedInInformation === '1') {
-    setIsLoggedIn(true);
-   }
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
   }, [])
 
+
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{
+      isLoggedIn: false,
+    }}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
